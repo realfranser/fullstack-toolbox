06816d4441729367d2b1305950e9a18668ed252f
realfranser <serranoarresejavier@gmail.com>
2021-10-18 15:54:48 +0200

    created product model and entity

--- back/inventory-product-service/src/entities/product.go
+++ back/inventory-product-service/src/entities/product.go
@@ -3,12 +3,13 @@ package entities
 import "fmt"
 
 type Product struct {
-	Id int64
-	Name string
-	Price float64
+	Id       int64
+	Name     string
+	Price    float64
 	Quantity int64
 }
 
 func (product Product) ToString() string {
-	return fmt.Sprintf("id: %d/nname: %s/nprice: %0.1f/n
-		quantity: %d", product.Id, product.Name, product.Price, product.Quantity)
+	return fmt.Sprintf("id: %d/nname: %s/nprice: %0.1f/nquantity: %d",
+		product.Id, product.Name, product.Price, product.Quantity)
+}
