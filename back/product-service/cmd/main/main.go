ffc8f1b6b40aade7b50c4dc47979082625a04c92
realfranser <serranoarresejavier@gmail.com>
2021-11-04 14:21:47 +0100

    main.go

--- back/product-service/cmd/main/main.go
+++ back/product-service/cmd/main/main.go
@@ -1,5 +1,17 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/realfranser/fullstack-toolbox/back/product-service/pkg/routes"
 )
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:18002", r))
+}
