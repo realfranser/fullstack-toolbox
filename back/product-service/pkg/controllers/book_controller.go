54e143912d836374f7816afaa5f7b0eb9f4fb2bd
realfranser <serranoarresejavier@gmail.com>
2021-11-05 12:29:08 +0100

    fix getAllBooks func

--- back/product-service/pkg/controllers/book_controller.go
+++ back/product-service/pkg/controllers/book_controller.go
@@ -35,7 +35,7 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
-func CreateBook (w http.ResponseWriter, r *http.Request) {
+func CreateBook(w http.ResponseWriter, r *http.Request) {
 	var createBook = &models.Book{}
 	utils.ParseBody(r, createBook)
 	b := createBook.CreateBook()
@@ -44,7 +44,7 @@ func CreateBook (w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
-func DeleteBook (w http.ResponseWriter, r *http.Request) {
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	bookId := vars["bookId"]
 	ID, err := strconv.ParseInt(bookId, 0, 0)
@@ -58,7 +58,7 @@ func DeleteBook (w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
-func UpdateBook (w http.ResponseWriter, r *http.Request) {
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	var updateBook = &models.Book{}
 	utils.ParseBody(r, updateBook)
 	vars := mux.Vars(r)
--- back/product-service/pkg/routes/book_routes.go
+++ back/product-service/pkg/routes/book_routes.go
@@ -7,7 +7,7 @@ import (
 
 var RegisterBookRoutes = func(router *mux.Router){
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/", controllers.GetAllBooks).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
