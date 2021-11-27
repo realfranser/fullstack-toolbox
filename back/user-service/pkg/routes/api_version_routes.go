bd0f478d0bce39d06b7e922d9599f0b2bd5c07ff
realfranser <serranoarresejavier@gmail.com>
2021-11-27 13:31:03 +0100

    global enrouting finished

--- back/user-service/pkg/routes/api_version_routes.go
+++ back/user-service/pkg/routes/api_version_routes.go
@@ -2,8 +2,10 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
+	v1 "github.com/realfranser/fullstack-toolbox/back/user-service/pkg/routes/v1"
 )
 
 var RegisterV1Routes = func(router *mux.Router) {
-		
+	v1.RegisterAuthRoutes(router)
+	v1.RegisterUserRoutes(router)
 }
