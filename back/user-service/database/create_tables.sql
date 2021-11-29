6eed3b44f1d33b5ccc4a1adb536cfaca7c62dfd5
realfranser <serranoarresejavier@gmail.com>
2021-11-29 13:19:05 +0100

    database created, table users created

---
+++ back/user-service/database/create_tables.sql
@@ -0,0 +1,16 @@
+USE user_service;
+
+CREATE TABLE `users`
+(
+	id INT AUTO_INCREMENT,
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	phone VARCHAR(30) NOT NULL,
+	token VARCHAR(100),
+	user_type VARCHAR(10) NOT NULL,
+	refresh_token VARCHAR(100) NOT NULL,
+	-- Properties
+	PRIMARY KEY(id)
+);
