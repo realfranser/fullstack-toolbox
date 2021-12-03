fd5991bfb418a5d1904f054718ee111b1e0fc0c5
realfranser <serranoarresejavier@gmail.com>
2021-12-03 10:38:23 +0100

    pagination helper indented

--- back/user-service/pkg/helpers/pagination_helper.go
+++ back/user-service/pkg/helpers/pagination_helper.go
@@ -9,22 +9,22 @@ import (
 
 func Paginate(r *http.Request) (offset int, pageSize int){
 	vars := mux.Vars(r)
-		queryPageSize := vars["pageSize"]
-		queryPage := vars["page"]
-		queryOffset := vars["offset"]
-		pageSizeResult, err := strconv.ParseInt(queryPageSize, 0, 0)
-		if err != nil || pageSize < 1{
-			pageSize = 10
-		}
-		page, err := strconv.ParseInt(queryPage, 0, 0)
-		if err != nil || page < 1 {
-			page = 1
-		}
-		offsetResult := (page -1) * pageSizeResult
-		specificOffset, err := strconv.ParseInt(queryOffset, 0, 0)
-		if err == nil && specificOffset > 0 {
-			offsetResult = specificOffset
-		}
+	queryPageSize := vars["pageSize"]
+	queryPage := vars["page"]
+	queryOffset := vars["offset"]
+	pageSizeResult, err := strconv.ParseInt(queryPageSize, 0, 0)
+	if err != nil || pageSize < 1{
+		pageSize = 10
+	}
+	page, err := strconv.ParseInt(queryPage, 0, 0)
+	if err != nil || page < 1 {
+		page = 1
+	}
+	offsetResult := (page -1) * pageSizeResult
+	specificOffset, err := strconv.ParseInt(queryOffset, 0, 0)
+	if err == nil && specificOffset > 0 {
+		offsetResult = specificOffset
+	}
 
 	return int(offsetResult), int(pageSizeResult)
 }
