cb0c1ed09e8924e4315a8ad6d0ec78912dfaa2ed
realfranser <serranoarresejavier@gmail.com>
2021-12-03 10:10:29 +0100

    better access to db

--- back/user-service/pkg/config/constants_example.go
+++ back/user-service/pkg/config/constants_example.go
@@ -1,7 +1,12 @@
 package config
 
 const (
-	// ServicePort = 0 // Type your prefered service port as an integer
-	// ServiceName = "" // Type your prefered service name as a string
-	// SecretKeyExample = "" // Type your secret key as a string
+	// ServicePort = int
+	// ServiceName = string
+	// SecretKey = string // Used for encryption: https://github.com/realfranser/fullstack-toolbox/blob/32ac8d1698ab1716914d217af3cd2e4b8028b17d/back/user-service/pkg/helpers/token_helper.go#L22
+	// DBHost = string
+	// DBPort = int
+	// DBUser = string
+	// DBPassword = string
+	// DBName = string
 )
