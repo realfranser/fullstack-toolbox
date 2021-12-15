f2626d3cd96b1346db9c20ae65e7c990be7dbb40
realfranser <serranoarresejavier@gmail.com>
2021-12-15 20:34:18 +0100

    validation bug fixed

--- back/user-service/pkg/models/user_model.go
+++ back/user-service/pkg/models/user_model.go
@@ -12,13 +12,13 @@ var userDB *gorm.DB
 
 type User struct {
 	gorm.Model
-	First_name			*string		`json:"first_name" validate:"required, min=2, max=100"`
-	Last_name				*string		`json:"last_name" validate:"required, min=2, max=100"`
-	Password				*string		`json:"password" validate:"required, min=6, max=100"`
-	Email						*string		`json:"email" validate:"email, required"`
+	First_name			*string		`json:"first_name" validate:"required,min=2,max=100"`
+	Last_name				*string		`json:"last_name" validate:"required,min=2,max=100"`
+	Password				*string		`json:"password" validate:"required,min=6,max=100"`
+	Email						*string		`json:"email" validate:"email,required"`
 	Phone						*string		`json:"phone" validate:"required"`
 	Token						*string		`json:"token"`
-	User_type				*string		`json:"user_type" validate:"required, eq=admin|eq=user"`
+	User_type				*string		`json:"user_type" validate:"required,eq=admin|eq=user"`
 	Refresh_token		*string		`json:"refresh_token"`
 	User_id					string		`json:"user_id"`
 }
