394d969f8ea4bee87335c016dca03203d6ff481b
realfranser <serranoarresejavier@gmail.com>
2022-01-02 00:55:49 +0100

    routes created for App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
-import { Cart } from './pages';
+import {
+  Navigate,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import { Cart, Home, Login, Product, ProductList, Register } from './pages';
 
 const App = () => {
-  return <Cart />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="products/:category" element={<ProductList />} />
+        <Route path="product/:id" element={<Product />} />
+        <Route path="cart" element={<Cart />} />
+        {/*TODO: Change false to user (from redux*/}
+        <Route path="login" element={false ? <Navigate to="/" /> : <Login />} />
+        {/*TODO: Change false to user (from redux*/}
+        <Route
+          path="register"
+          element={false ? <Navigate to="/" /> : <Register />}
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
