624efb96f25f8a22dbb08b1787e87861fdc856c0
realfranser <serranoarresejavier@gmail.com>
2022-01-06 23:34:17 +0100

    created BASE_URL constant

--- front/src/constants/services.ts
+++ front/src/constants/services.ts
@@ -1,7 +1,9 @@
 const HOST = 'http://localhost';
 const PORT = 18000;
+const BASE_URL = `${HOST}:${PORT}/api`;
 
 export const API = {
-  V1_PRODUCTS: `${HOST}:${PORT}/api/v1/products`,
-  V1_USERS: `${HOST}:${PORT}/api/v1/users`,
+  V1_PRODUCT: `${BASE_URL}/v1/product`,
+  V1_PRODUCTS: `${BASE_URL}/v1/products`,
+  V1_USERS: `${BASE_URL}/v1/users`,
 };
