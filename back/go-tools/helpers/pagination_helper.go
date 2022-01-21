e2a720bb4c5714f429de6c3c412ed427ef0f31bd
realfranser <serranoarresejavier@gmail.com>
2022-01-21 11:26:37 +0100

    added all tools

--- back/go-tools/helpers/pagination_helper.go
+++ back/go-tools/helpers/pagination_helper.go
@@ -7,11 +7,11 @@ import (
 func Paginate(paginationParams *models.Pagination) (offset int, pageSize int){
 	/* Check for null or invalid page size values */
 	if paginationParams.PageSize <= 0 {
-		paginationParams.PageSize = models.DefaultPageSize
+		paginationParams.PageSize = models.DEFAULT_PAGE_SIZE
 	}
 	/* Check for null or invalid page index values */
 	if paginationParams.PageIndex < 1 {
-		paginationParams.PageIndex = models.DefaultPageIndex
+		paginationParams.PageIndex = models.DEFAULT_PAGE_INDEX
 	}
 	/* If the offset is not specified, it will be determined by the page size and the page index */
 	offsetResult := (paginationParams.PageIndex -1) * paginationParams.PageSize
