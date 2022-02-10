51f62f51ded0ac2e79f4747d16c370443af8abbb
realfranser <serranoarresejavier@gmail.com>
2022-02-10 22:01:32 +0100

    closes #31; homepage products added to database

---
+++ back/product-service/database/create_homepage_products.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+	products (name, category, price, stock, description)
+VALUES
+	('guitar_tshirt', 'homepage', 1, 0, 'guitar_tshirt'),
+	('afro_tshirt', 'homepage', 2, 1, 'afro_tshirt'),
+	('colorful_shirt', 'homepage', 3, 2, 'colorful_shirt'),
+	('summer_dress', 'homepage', 4, 3, 'summer_dress'),
+	('bio_bag', 'homepage', 5, 4, 'bio_bag'),
+	('cat_cap', 'homepage', 6, 5, 'cat_cap'),
+	('brown_coat', 'homepage', 7, 6, 'brown_coat');
